a6b003b734fe9430cb1adef02895a8264daed67d
阿坤 <826964882@qq.com>
2021-10-31 17:34:30 +0800

    reserveSeats
    #7

---
+++ miniprogram-8/miniprogram/pages/demo1/demo1.js
@@ -0,0 +1,173 @@
+// pages/demo1/demo1.js
+const db=wx.cloud.database();
+var new_data=[] //定义一个空的集合，将查到的数据存放其中
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    userName:' ',
+    userId:"",
+    array:['一楼报纸阅览区','二楼大厅','二楼中文社科图书阅览区一区','二楼中文社科图书阅览区二区'],
+    index:5,
+    index:0
+  },
+  userNameInput:function(e){
+    this.setData({
+      userName:e.detail.value
+    })
+  },
+  userIdInput:function(e){
+    this.setData({
+      userId:e.detail.value
+    })
+  },
+  telePhoneInput:function(e){
+    this.setData({
+      telePhone:e.detail.value
+    })
+  },
+  //获取用户输入的密码
+  loginBtnClick: function (e) {
+    console.log("用户名："+this.data.userName+" 密码：" +this.data.userId+"联系方式"+this.data.telePhone);
+    //console.log(this.data.userId);
+  },
+  bindPickerChange:function(e){
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index: e.detail.value
+    })
+  },
+  bindPickerChange:function(e){
+    db.collection("seats").where({
+      state:1
+      // skzhID: _.in(data.wdhID)
+    }).count().then(async res => {
+      let total = res.total;
+      console.log('测试res.total', res.total)
+      //   // 计算需分几次取
+      const batchTimes = Math.ceil(total / 20)
+      // 循环读取数据库，并将读取的数据存放至new_data
+      for (let i = 0; i < batchTimes; i++) {
+        await db.where({
+          state:1
+          //skzhID: _.in(data.wdhID)
+        }).skip(i * 20).limit(20).get().then(async res => {
+          new_data = new_data.concat(res.data)
+          console.log(new_data)
+          //let old_data = that.data.allRecords
+          //           that.setData({
+          //         allRecords : new_data.concat(new_data)
+          // })
+        })
+      }       
+      var new_data_json = JSON.stringify(new_data) //将数据库查询的数据集合转换为json用于传递给下一界面
+      // wx.navigateTo({
+      //   url: '../jieguozhanshi/jieguozhanshi?banner=' + new_data_json //将数据传递给指定界面,用../跳转到两一个文件夹的页面
+      // })
+    })
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index: e.detail.value
+    })
+  },
+  //据库获取所有记录
+  getData(){
+  db.collection("seats").where({
+    state:1
+    // skzhID: _.in(data.wdhID)
+  }).count().then(async res => {
+    let total = res.total;
+    console.log('测试res.total', res.total)
+    //   // 计算需分几次取
+    const batchTimes = Math.ceil(total / 20)
+    // 循环读取数据库，并将读取的数据存放至new_data
+    for (let i = 0; i < batchTimes; i++) {
+      await db.collection("seats").where({
+        state:1
+        //skzhID: _.in(data.wdhID)
+      }).skip(i * 20).limit(20).get().then(async res => {
+        new_data = new_data.concat(res.data)
+        console.log(new_data)
+        //let old_data = that.data.allRecords
+        //           that.setData({
+        //         allRecords : new_data.concat(new_data)
+        // })
+      })
+    }       
+    var new_data_json = JSON.stringify(new_data) //将数据库查询的数据集合转换为json用于传递给下一界面
+    // wx.navigateTo({
+    //   url: '../jieguozhanshi/jieguozhanshi?banner=' + new_data_json //将数据传递给指定界面,用../跳转到两一个文件夹的页面
+    // })
+  })
+},
+
+
+
+
+  // updateData(){
+  //   db.collection("seats").where({num:1}).update({
+  //     data:{
+  //       num:3
+  //     }
+  //   }).then(res=>{
+  //     console.log(res)
+  //   })
+ // },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ miniprogram-8/miniprogram/pages/index/index.js
@@ -0,0 +1,101 @@
+const db=wx.cloud.database()//连接数据库
+Page({
+
+ reserveBtn:function(options){
+   wx.navigateTo({
+     url:'../demo1/demo1'
+   })
+ },
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    dataObj:""
+  },
+  getData(){
+    // db.collection("demolist").get().then(res=>{
+    //   //console.log(res)
+    //   this.setData({
+    //     dataObj:res.data
+    //   })
+    // })
+    //查询
+  //   db.collection('seats').where({
+  //     num:12
+  //   }).get()
+  //   .then(res=>{
+  //     console.log(res)
+  //   })
+  // },
+  // addData(){
+  //   wx.showLoading({
+  //     title:'数据加载中...',
+  //     mask:true
+  //   })
+  //   db.collection("seats").add({
+  //   data:{
+  //     location:"二楼",
+  //     num:1,
+  //     state:1
+  //   }
+  // }).then(res=>{
+  //   console.log(res)
+  //   wx.hideLoading()
+  //   })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
